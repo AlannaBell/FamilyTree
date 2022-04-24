5074d324158e46a9163a4d0fdd25a5a2f2560486
AlannaBell <104325551+AlannaBell@users.noreply.github.com>
2022-04-24 23:36:38 +0100

    Add files via upload

---
+++ FamilyTreeAssignment/FamilyTree_Step1_B00387188_/src/FamilyTree.java
@@ -0,0 +1,157 @@
+
+public class FamilyTree {
+       public class FamilyMemberNotFoundException extends Exception{}
+
+    private class FamilyTreeNode {
+        private String name;
+        private FamilyTreeNode ancestor;
+        private FamilyTreeNode sibling;
+        private FamilyTreeNode children;
+        private FamilyTreeNode partner;
+    }
+
+    private FamilyTreeNode ancestor;
+    private FamilyTreeNode current;
+
+//Sets up exception for a non unique name being input
+    public static class NameNotUniqueException extends Exception {}
+
+//constructor   
+    public FamilyTree(String ancestorName, String partnername) {
+        this.ancestor = new FamilyTreeNode();
+        this.ancestor.name = ancestorName;
+        this.current = this.ancestor;
+        addPartner(partnername);
+    }
+
+ //using toString allows a string to be returned that shows the specified inputs 
+    public String toString() {
+        String familyDetails = new String();
+        familyDetails += this.ancestor.name +  getPartner(this.ancestor);
+        FamilyTreeNode familymember = this.ancestor.children;
+        if (familymember == null) {
+            familyDetails += "  has no children\n";        
+        } else {
+            while (familymember != null) {
+                familyDetails += "  " + familymember.name + getPartner(familymember);
+                familyDetails += this.getChildren(familymember);
+                familymember = familymember.sibling;
+            }
+        }
+        return familyDetails;
+    }
+
+//gets information on child
+    private String getChildren(FamilyTreeNode familymember) {
+        String childrenDetails = new String();
+        familymember = familymember.children;
+        if (familymember == null) {
+            childrenDetails += "    has no children\n";
+        } else {
+            while (familymember != null) {
+                childrenDetails += "    " + familymember.name + getPartner(familymember) +  "\n";
+                familymember = familymember.sibling;
+            }
+        }
+        return childrenDetails;
+    }
+
+//ability to add child
+    public void addChild(String name) {
+        FamilyTreeNode familymember = new FamilyTreeNode();
+        familymember.name = name;
+        familymember.ancestor = this.current;
+        if (this.current.children == null) {
+            this.current.children = familymember;
+               if(this.current.partner != null){
+                    this.current = this.current.partner;
+                    this.current.children = familymember;
+        } else {
+            FamilyTreeNode next = this.current.children;
+            while (next.sibling != null) {
+                next = next.sibling;
+            }
+            next.sibling = familymember;
+        }
+      }
+    }
+    
+ //ability to add partner
+    public void addPartner(String name) {
+        FamilyTreeNode familymember = new FamilyTreeNode();
+        familymember.name = name;
+        familymember.ancestor = this.current;
+        this.current.partner = familymember; }    
+    
+//gets information on partner
+    private String getPartner(FamilyTreeNode familymember) {
+        String partnerDetails = new String();
+        familymember = familymember.partner;
+        if (familymember == null) {
+            partnerDetails += "    has no partner\n";
+        } else {
+            partnerDetails += " partner " + familymember.name + "\n";
+        }
+        return partnerDetails;
+    }
+
+
+ //finds family member   
+    public void findFamilyMember(String name) throws FamilyMemberNotFoundException{
+        FamilyTreeNode familymember;
+        FamilyTreeNode children;
+        if (name.equalsIgnoreCase(this.ancestor.name)) {
+            familymember = this.ancestor;
+        } else {
+            if(this.ancestor.children==null)
+                throw new FamilyMemberNotFoundException();
+            familymember = this.checkChildren(name, this.ancestor);
+            if (familymember == null) {
+                children = this.ancestor.children;
+                while (familymember==null) {
+                    familymember = this.checkChildren(name, children);
+                    if (familymember == null) {
+                        children = children.sibling;
+                        if (children == null) {
+                            throw new FamilyMemberNotFoundException();
+                        }
+                    }
+                }
+            }
+        }
+        this.current=familymember;
+        getCurrent();
+    }
+
+    
+    private FamilyTreeNode checkChildren(String name, FamilyTreeNode familymember) {
+        familymember=familymember.children;
+        Boolean searching=familymember!=null;
+        while (searching) {
+            if (name.equalsIgnoreCase(familymember.name)) {
+                searching = false;
+            } else {
+                familymember = familymember.sibling;
+                if (familymember == null) {
+                    searching = false;
+                }
+            }
+        }
+        return familymember;
+    }
+
+//so that currently selected family member can be displayed
+    public String getCurrent() {
+        String currentDetails = new String();
+        currentDetails += this.current.name +getPartner(this.current);
+        if (this.current.ancestor != null) {
+            currentDetails += " is the child of " + this.current.ancestor.name + '\n';
+        } else {
+            currentDetails += " is the ancestor\n";
+        }
+        currentDetails += this.getChildren(this.current);
+        return currentDetails;
+    }
+
+}
+
---
+++ FamilyTreeAssignment/FamilyTree_Step2_B00387188_1/src/FamilyTreeTest.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FamilyTreeTest {
+       public static void main(String[] args) {
+        //user must input an ancestors name to begin the family tree
+        String name = Input.getString("input the ancestors name: ");
+        String partnername = Input.getString("input their partner's name: ");
+        
+        //creates an array/list of names
+        List namesFamily = new ArrayList();
+        
+        FamilyTree familytree = new FamilyTree(name,partnername);
+        Integer option;
+        //a menu is brought up with options that the user can choose from
+        do {
+            System.out.println("0: quit");
+            System.out.println("1: add child to current");
+            System.out.println("2: add partner to ancestor");           
+            System.out.println("3: find family member");
+            System.out.println("4: display current");
+            System.out.println("5: display familytree");
+            option = Input.getInteger("input option: ");
+            switch (option) {
+                case 1:
+                    //user inputs the name of the child
+                    name = Input.getString("input the child's name: ");
+                        try{
+                            //checks if name is unique
+                            if(namesFamily.contains(name)){
+                                throw new FamilyTree.NameNotUniqueException();
+                            } else {
+                                familytree.addChild(name);
+                                namesFamily.add(name);
+                            }
+                        } catch(FamilyTree.NameNotUniqueException e){
+                            System.out.println("CHILD'S NAME IS NOT UNIQUE");
+                        }
+                    break;
+                case 2: 
+                    //user inputs the partners name
+                    try{
+                        name = Input.getString("input the partner's name: ");
+                        familytree.addPartner(name);
+                    // checks if person already has partner
+                    } catch(FamilyTree.AlreadyGotAPartnerException e){
+                        System.out.println("ALREADY HAS A PARTNER");
+                    }
+                    break;
+                case 3:
+                    //user inputs family members name to find them in the family tree
+                    name = Input.getString("input the family member's name: ");
+                    try{
+                        familytree.findFamilyMember(name);
+                        System.out.println(familytree.getCurrent());
+                    } catch(FamilyTree.FamilyMemberNotFoundException e){
+                        System.out.println("not found");
+                    } 
+                    break;
+                case 4:
+                    //current family member is printed
+                    System.out.println(familytree.getCurrent());
+                    break;
+                case 5:
+                    //family tree is printed
+                    System.out.println(familytree);
+                    break;
+            }
+            //if option 0 is selected then program closes
+        } while (option != 0);
+    }
+
+}
+
